5d7748091ad079adbe9be39a66f0e7d339518376
Andress <andressbotina96@gmail.com>
2020-04-13 12:42:10 -0500

    acabe con el formulario
    que baboso soy XD

--- JavaScrip/formulario.js
+++ JavaScrip/formulario.js
@@ -2,6 +2,8 @@ var formulario;
 var elementos;
 var boton;
 var validarNombre;
+var validarRadio;
+var validarCheckbox;
 var validar;
 
 
@@ -11,14 +13,34 @@ var validar;
  elementos = formulario.elements;
  boton= document.getElementById('btn');
 
-validarNombre=function () {
+validarNombre=function (e) {
 if (formulario.nombre.value==0) {
   alert("Completa el campo Nombre");
+  e.preventDefault();
 }
+}
+validarRadio = function (e) {
+  if (formulario.genero[0].checked == true || formulario.genero[1].checked == true) {
+  }else {
+    alert("completa el campo del genero");
+    e.preventDefault();
+  }
 }
 
- validar = function () {
-    validarNombre();
+validarCheckbox=function (e) {
+  if (formulario.Terminos.checked==true) {
+}else {
+    alert(" no has completado el campo de terminos y condiciones");
+    e.preventDefault();
+
+  }
+
+}
+
+ validar = function (e) {
+    validarNombre(e);
+    validarRadio(e);
+    validarCheckbox(e);
  }
 
  formulario.addEventListener("submit",validar);
